5b7c9761845c0bc404e19936f24e3ffa21ae12cc
Akasaransh <51713339+Akasaransh@users.noreply.github.com>
2022-10-30 00:24:06 +0530

    added sleep nd var name change

--- latencyFix.py
+++ latencyFix.py
@@ -1,10 +1,11 @@
 from time import sleep
 from datetime import datetime
 
-success = []
+successTrue = []
 while 1:
     dateSTR = datetime.now().strftime('%H:%M')
     if (dateSTR) == (newnametest) and not dateSTR in done:
         done.append(dateSTR)
         os.system("mplayer -ao alsa:device=bluealsa /home/pi/test.mp3")
     print(dateSTR)
+    sleep(1)
